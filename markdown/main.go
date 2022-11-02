bac6ef72d2e4d25cf6496e4f33a7926182e40ff4
Redari-Es <905422030@qq.com>
2022-11-03 02:03:20 +0800

    markdown editor quick start need to continued

---
+++ markdown/main.go
@@ -0,0 +1,50 @@
+package main
+
+import (
+	"fyne.io/fyne/v2"
+	"fyne.io/fyne/v2/app"
+
+	"fyne.io/fyne/v2/container"
+	"fyne.io/fyne/v2/widget"
+)
+
+type config struct {
+	EditWidget    *widget.Entry
+	PreviewWidget *widget.RichText
+	CurrentFile   fyne.URI
+	SaveMenuItem  *fyne.MenuItem
+}
+
+var cfg config
+
+func main() {
+	// create a fyne app
+	a := app.New()
+	// create a window for the app
+	win := a.NewWindow("Markdown")
+
+	//get the user interface
+	edit, preview := cfg.makeUI()
+
+	// set the content of the window
+	win.SetContent(container.NewHSplit(edit, preview))
+
+	//show window add run app
+	win.Resize(fyne.Size{Width: 800, Height: 500})
+
+	//focus on the ceter window
+	win.CenterOnScreen()
+	win.ShowAndRun()
+
+}
+
+func (app *config) makeUI() (*widget.Entry, *widget.RichText) {
+	edit := widget.NewMultiLineEntry()
+	preview := widget.NewRichTextFromMarkdown("")
+	app.EditWidget = edit
+	app.PreviewWidget = preview
+
+	//listener
+	edit.OnChanged = preview.ParseMarkdown
+	return edit, preview
+}
